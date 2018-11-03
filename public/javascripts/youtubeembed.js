9f9ca17f05bf35a0889ce5b33327303d15bd34d2
Svetlitski <kevin_svetlitski@berkeley.edu>
2018-11-03 15:17:55 -0700

    Now able to embed any youtube video.

---
+++ public/javascripts/youtubeembed.js
@@ -0,0 +1,14 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+	player = new YT.Player('ytplayer', {
+		height: '360',
+		width: '640',
+		videoId: vidId
+	});
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var url = require("url");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:vidId', function(req, res, next) {
+	var vidId = url.parse(req.url).pathname;
+	console.log(vidId);
+	res.render('index', req.params);
 });
 
 module.exports = router;
